69e90d9c1fbdbb495dbb8f136db0710cb80d3e32
Fabian Rosales (Frosquivel Developer) <fabianr7593@gmail.com>
2024-05-18 17:58:58 -0600

    Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@
 module.exports = {
     "type": "mysql",
     "host": "localhost",
-    "port": 3307,
+    "port": 3306,
     "username": "root",
     "password": "",
     "database": "easy_api_land_db",
